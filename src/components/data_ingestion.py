decdf5fd373bb381bbe2b57be29aacde735bdc0b
Chandrashekar <chandrashekarcp16@gmail.com>
2023-05-19 16:12:31 +0530

    Ingestion and transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -55,8 +55,8 @@ if __name__=='__main__':
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    train_arr,test_arr=data_transformation.initaite_data_transformation(train_data,test_data)
-    
+    train_arr,test_arr,_=data_transformation.initaite_data_transformation(train_data,test_data)
+    print("Data Ingestion Complete!")
 
     
      
